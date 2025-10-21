c7519f4685aeda033ec946cbcdc7e3c8d2d0d3ce
Mindbricks Admin <genesis@mindbricks.com>
2025-10-21 18:52:28 +0000

    Add/update service: hotelbackend-notification

---
+++ hotelbackend-notification/src/listeners/base/sms.listener.js
@@ -0,0 +1,35 @@
+const kafka = require("../../utils/kafka.client.js");
+const { loadSmsProvider } = require("../../utils/provider.loader.js");
+const consumer = kafka.consumer({
+  groupId: `hotelbackend-notification-service-sms-consumer`,
+});
+
+// Load the sms provider based on the environment variable
+const provider = loadSmsProvider();
+const runSmsSenderListener = async () => {
+  await consumer.connect();
+  await consumer.subscribe({
+    topic: `hotelbackend-notification-service-notification-sms`,
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      try {
+        console.log(
+          `Received message on ${topic}: ${message.value.toString()}`,
+        );
+        const noticeData = JSON.parse(message.value.toString());
+        await provider.send(noticeData);
+      } catch (error) {
+        //**errorLog
+        console.error(
+          `hotelbackend-notification-service-notification-sms: `,
+          error,
+        );
+      }
+    },
+  });
+};
+
+module.exports = runSmsSenderListener;
---
+++ hotelbackend-notification/src/listeners/index.js
@@ -0,0 +1,15 @@
+const startBaseListeners = require("./base");
+const userVerificationListeners = require("./userVerification.listener");
+const userResetPasswordListeners = require("./userResetPassword.listener");
+
+const startListener = async () => {
+  try {
+    await startBaseListeners();
+    await userVerificationListeners();
+    await userResetPasswordListeners();
+  } catch (error) {
+    //**errorLog
+  }
+};
+
+module.exports = startListener;
